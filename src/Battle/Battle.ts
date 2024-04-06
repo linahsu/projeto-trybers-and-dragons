b5ed4df62d0105e2d6a88c225ebb9c479ff69492
Lina Hsu <linahthsu@gmail.com>
2024-04-06 13:16:13 -0300

    Feat - implements class PVP

--- src/Battle/Battle.ts
+++ src/Battle/Battle.ts
@@ -1,7 +1,11 @@
-import Fighter from '../Fighter/Fighter';
+import Character from '../Character';
 
 abstract class Battle {
-  constructor(protected player: Fighter) { }
+  constructor(private _player: Character) { }
+
+  get player() {
+    return this._player;
+  }
 
   // Should return 1 if player wins, -1 otherwise
   fight(): number {
---
+++ src/Battle/PVP.ts
@@ -0,0 +1,53 @@
+import Character from '../Character';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  private _player2: Character;
+
+  constructor(
+    player1: Character,
+    player2: Character,
+  ) {
+    super(player1);
+    this._player2 = player2;
+  }
+
+  get player1() {
+    return super.player;
+  }
+
+  get player2() {
+    return this._player2;
+  }
+
+  static playerAttacks(
+    firstPlayer: Character,
+    secondPlayer: Character,
+  ): void {
+    if (firstPlayer.lifePoints !== -1) {
+      firstPlayer.attack(secondPlayer);
+      secondPlayer.receiveDamage(firstPlayer.strength);
+    }
+    if (secondPlayer.lifePoints !== -1) {
+      secondPlayer.attack(firstPlayer);
+      firstPlayer.receiveDamage(secondPlayer.strength);
+    }
+  }
+
+  fight(): number {
+    const player1 = super.player;
+    const player2 = this._player2;
+
+    while (player1.lifePoints !== -1 || player2.lifePoints !== -1) {
+      PVP.playerAttacks(player1, player2);
+    }
+
+    return player1.lifePoints === -1 ? -1 : 1;
+  }
+}
+
+const player1 = new Character('Player 1');
+const player2 = new Character('Player 2');
+player1.levelUp();
+const pvpBattle = new PVP(player1, player2);
+console.log(pvpBattle.fight());
